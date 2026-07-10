16f293c9f2391978c181ee8389dcc84753616c13
Harish <rathwameet115@gmail.com>
2026-07-10 20:41:07 +0530

    Stop tracking .env file and update gitignore

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-from flask import Flask
+from flask import Flask ,sessions,render_template
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def __init__():
+    return "app is running"
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
+
+
+
--- auth/models.py
+++ auth/models.py
@@ -1,11 +1,27 @@
 import bcrypt 
 from db.mongodb import db
-from datetime  import datetime
-
+from datetime import datetime, timezone
 
 class user(db.Document):
-    meta={
-        "collection":"user",
-        "indexes":"emails"
+    meta = {
+    "collection": "users",
+    "indexes": ["email"]
     }
-    name=db.StringField()
+    name = db.StringField(required=True, max_length=50)
+    email = db.EmailField(required=True, unique=True)
+    password_hash = db.StringField(required=True)
+
+    is_active = db.BooleanField(default=True)
+    created_at = db.DateTimeField(default=lambda: datetime.now(timezone.utc))
+    updated_at = db.DateTimeField(default=lambda: datetime.now(timezone.utc))
+
+
+    def set_password(self, plain_text_password):
+        salt = bcrypt.gensalt()
+        self.password_hash = bcrypt.hashpw(plain_text_password.encode("utf-8"), salt).decode("utf-8")
+    def check_password(self,plain_text_password):
+         return bcrypt.checkpw(plain_text_password.encode('utf-8'), self.password_hash.encode('utf-8'))
+
+    def save(self, *args, **kwargs):
+        self.updated_at=datetime.now(timezone.utc)
+        return super(user,self).save(*args,**kwargs)
